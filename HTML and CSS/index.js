cf274636989ec137046c04d50eba0be1e4851ada
Clay Logsdon <ClayLogsdon@users.noreply.github.com>
2022-03-18 18:57:20 -0400

    [Add] JS

--- HTML and CSS/index.js
+++ HTML and CSS/index.js
@@ -3,3 +3,28 @@ var audio = new Audio("../graphics/Ghibli-Jazz-Piano.mp3");
 document.onclick = function() {
   audio.play();
 }
+
+function zodiacInput() {
+  const birthmonth = document.getElementById("month").value;
+  const birthday = document.getElementById("day").value;
+}
+
+const submitButton = document.querySelector('birthday-selector');
+submitButton.addEventListener('click', function(event){
+  event.preventDefault();
+  zodiac();
+});
+
+  const date = document.getElementById('birthday').value;
+
+  const getBday = () => {
+
+    const date = document.getElementById('birthday').value;
+ 
+     if (date.length <= 0) return;
+ 
+     month = parseInt(date.substring(5,7));
+     day = parseInt(date.substring(8,10));
+ 
+     bDay();
+ };
